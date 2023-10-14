82e6d3e41e5fb47cdc4898617141e2476b8edb13
Cicmanec <cicmanec@optisolutions.cz>
2023-10-14 22:55:35 +0200

    Added base php page.

---
+++ pihole/php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Example</title>
+    <link rel="icon" href="data:;base64,iVBORw0KGgo="/>
+  </head>
+  <body>
+
+    <h1>Test</h1>
+    <h1><?php echo 'Hello world!'; ?></h1>
+
+  </body>
+</html>
