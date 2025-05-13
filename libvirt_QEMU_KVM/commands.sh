902ae9b0d330f036d91e39c2016bfed8039ef6c5
MartinCicmanec <48385658+MartinCicmanec@users.noreply.github.com>
2025-05-13 15:39:24 +0200

    Update commands.sh
    added virt-sparsify command.

--- libvirt_QEMU_KVM/commands.sh
+++ libvirt_QEMU_KVM/commands.sh
@@ -1,4 +1,7 @@
 # Shink image
 qemu-img convert -c -O qcow2 /home/martin/Downloads/Win11SandboxBckp.qcow2 /home/martin/Downloads/Win11Sandbox.qcow2
+# Another variant of shrinking
+sudo apt install libguestfs-tools
+virt-sparsify --compress /home/martin/Downloads/Win11SandboxBckp.qcow2 /home/martin/Downloads/Win11Sandbox.qcow2
 # Create 100MB virtual disk to be mounted into vrt system.
 qemu-img create -f qcow2 lic.img 100m
